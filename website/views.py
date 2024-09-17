c161e6bd15f1d1fdb46e0a4d9126aa3f52549600
gatir <mm.pourdad@gmail.com>
2024-09-17 16:42:42 +0330

    created render html file

--- website/views.py
+++ website/views.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse, JsonResponse
 
 
 def index_view(request):
-    return HttpResponse('<h1>home page')
-
-
-def contact_view(request):
-    return HttpResponse('<h1>contact us')
+    return render(request, 'website/index.html')
 
 
 def about_view(request):
-    return HttpResponse('<h1>about us')
+    return render(request, 'website/about.html')
+
+
+def contact_view(request):
+    return render(request, 'website/contact.html')
